dbaa6e340d9128bf7523a4e964f0d97f3e6ca98a
JuliaHaller <114695788+JuliaHaller@users.noreply.github.com>
2022-12-05 20:09:40 -0800

    Colliders
    Added Colliders to most of the game objects in the scene. Added small buildings. Added but did not implement Movement Code

--- RustySignal/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ RustySignal/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,49 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 5.0f;
-	public float jumpForce = 5.0f;
-	public bool isOnGround = true;
-	private float horizontalInput;
-	private float forwardInput;
-	private Rigidbody playerRb;
+    [Header("Movement")]
+    public float moveSpeed;
 
+    public float groundDrag;
 
-	void Start()
-	{
-		
-		playerRb = GetComponent<Rigidbody>();
-	}
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
 
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
 
-	void Update()
-	{
-		//get player Input
-		horizontalInput = Input.GetAxis("Horizontal");
-		forwardInput = Input.GetAxis("Vertical");
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
 
-		//move the player forward
-		transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-		transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
 
-		//let the player jump
-		if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
-		{
-			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-			isOnGround = false;
-		}
+    public Transform orientation;
 
-	}
+    float horizontalInput;
+    float verticalInput;
 
-	private void OnCollisionEnter(Collision collision)
-	{
+    Vector3 moveDirection;
 
-		if(collision.gameObject.CompareTag("Ground"))
-		{
-			isOnGround = true;
-		}
-	}
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        // on ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // in air
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
 }
